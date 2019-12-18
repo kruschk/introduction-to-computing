8d0d407b5764ee5520c1a475d2224d49a4c7cc3c
Jonah Kruschke <jonah.kruschke@gmail.com>
2019-12-17 19:45:50 -0500

    Add completed exercises
    Some exercises may be missing. I will add the rest if I can find them.

---
+++ ch3/e3-10.scm
@@ -0,0 +1,11 @@
+; e3-10
+(define (absvalue x)
+  (if (< x 0)
+      (- x)
+      x))
+(define (bigger-magnitude x y)
+  (if (> (absvalue x)
+         (absvalue y))
+      x
+      y))
+(bigger-magnitude -15 99)
